a0f7a542f948698a8160da776479b068f8b1b5e1
math-uka <aforgary@gmail.com>
2025-05-17 19:13:11 +0800

    Create p1food.js

---
+++ 05tool/05self_learning_graph/js/p1food.js
@@ -0,0 +1,12 @@
+{
+    "01": "Apple pie",
+    "02": "Cake",
+    "03": "Chicken wing",
+    "04": "Egg",
+    "05": "French fries",
+    "06": "Hamburger",
+    "07": "Mushroom",
+    "08": "Pork chop",
+    "09": "Potato",
+    "10": "Sandwich"
+}
